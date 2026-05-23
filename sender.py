8209840cbb19f42c82de59b6e24bd7d576100cec
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-24 00:35:00 +0300

    Update sender.py

--- sender.py
+++ sender.py
@@ -7,7 +7,9 @@ CHAT_ID = os.getenv("CHAT_ID")
 def send_to_channel(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    requests.post(url, data={
+    r = requests.post(url, data={
         "chat_id": CHAT_ID,
         "text": msg
     })
+
+    print("RESPONSE:", r.text)
