8e09e46178fc848d564352cbcb877a37c8944c54
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-24 01:10:16 +0300

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,33 +1,13 @@
-import random
+import requests
+import os
 
-def analyze_match(match):
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
-    home = match["teams"]["home"]["name"]
-    away = match["teams"]["away"]["name"]
+def send_to_channel(msg):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    # basit ama stabil model
-    base = random.randint(45, 60)
-
-    # MS tahmini
-    if base > 55:
-        ms = "1"
-    elif base < 48:
-        ms = "2"
-    else:
-        ms = "X"
-
-    # KG
-    kg = "VAR" if random.random() > 0.5 else "YOK"
-
-    # 2.5
-    over25 = "ÜST" if random.random() > 0.5 else "ALT"
-
-    # ilk yarı
-    first_half = "ÜST" if random.random() > 0.55 else "ALT"
-
-    return {
-        "ms": ms,
-        "kg": kg,
-        "over25": over25,
-        "first_half": first_half
-    }
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": msg
+    })
