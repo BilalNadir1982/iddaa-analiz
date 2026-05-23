9909d32b561915657b904f3cc942cd05c8860c42
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-24 01:26:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,5 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")          # Telegram bot token
-CHANNEL_ID = os.getenv("CHANNEL_ID")        # @kanaladi veya -100xxxxxxxxxx
-
-# Scraping ayarları
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
-}
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+ODDS_API_KEY = os.getenv("ODDS_API_KEY")
