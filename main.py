ef92ea706ce77f9dfcc06e1e04922f44e088d9ec
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-24 00:45:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 from api import get_matches
+from signal import generate_signal
 from sender import send_to_channel
 
 def main():
     matches = get_matches()
 
-    if not matches:
-        send_to_channel("Bugün maç bulunamadı.")
-        return
+    sent = 0
 
-    msg = "🔥 Günün İddaa Analizi 🔥\n\n"
+    for match in matches:
+        msg = generate_signal(match)
 
-    for match in matches[:3]:
-        home = match["teams"]["home"]["name"]
-        away = match["teams"]["away"]["name"]
+        if msg:
+            send_to_channel(msg)
+            sent += 1
 
-        msg += f"⚽ {home} - {away}\n"
-        msg += "💡 Tahmin: KG VAR\n\n"
-
-    send_to_channel(msg)
+        if sent >= 3:
+            break
 
 if __name__ == "__main__":
     main()
