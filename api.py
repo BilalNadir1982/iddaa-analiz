06f62c9bdfcab6479aa772be1ed3807b622477e3
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-25 00:29:34 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,63 +1,17 @@
 import requests
-from datetime import datetime
-import pytz
+from config import API_KEY
 
-from config import API_KEY, TIMEZONE
+URL = "https://v3.football.api-sports.io/fixtures"
 
 HEADERS = {
     "x-apisports-key": API_KEY
 }
 
-BASE_URL = "https://v3.football.api-sports.io"
-
-# =========================================
-# BUGÜNÜN MAÇLARI
-# =========================================
-
 def get_matches():
 
-    try:
+    r = requests.get(URL, headers=HEADERS)
 
-        turkey = pytz.timezone(TIMEZONE)
-
-        today = datetime.now(turkey).strftime("%Y-%m-%d")
-
-        url = f"{BASE_URL}/fixtures"
-
-        params = {
-            "date": today,
-            "timezone": TIMEZONE
-        }
-
-        response = requests.get(
-            url,
-            headers=HEADERS,
-            params=params,
-            timeout=30
-        )
-
-        print("STATUS:", response.status_code)
-
-        data = response.json()
-
-        print("API RESPONSE:", data)
-
-        # =====================================
-        # RESPONSE YOKSA
-        # =====================================
-
-        if "response" not in data:
-            print("RESPONSE YOK")
-            return []
-
-        matches = data["response"]
-
-        print("TOPLAM MAÇ:", len(matches))
-
-        return matches
-
-    except Exception as e:
-
-        print("API ERROR:", e)
-
+    if r.status_code != 200:
         return []
+
+    return r.json().get("response", [])
