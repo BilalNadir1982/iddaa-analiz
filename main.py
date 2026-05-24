db325c71467e3057a7c28fcd1ce8127e5120197a
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-25 00:31:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,36 @@
 import time
-
-from telegram.ext import Updater, CallbackQueryHandler
-
 from api import get_matches
 from analyzer import analyze_match
-from sender import send_message, send_panel
-from panel import handle_buttons
+from sender import send_message
 
-sent_matches = set()
-
+sent = set()
 
 def main():
 
-    send_message("🚀 PRO BOT AKTİF")
-
-    send_panel()
-
-    updater = Updater("TELEGRAM_BOT_TOKEN", use_context=True)
-
-    dp = updater.dispatcher
-
-    # 🔥 BUTONLAR AKTİF
-    dp.add_handler(CallbackQueryHandler(handle_buttons))
-
-    updater.start_polling()
+    send_message("🚀 İDDIA PRO BOT AKTİF")
 
     while True:
 
         try:
-
             matches = get_matches()
-
-            count = 0
 
             for m in matches:
 
-                if count >= 15:
-                    break
-
                 mid = m["fixture"]["id"]
 
-                if mid in sent_matches:
+                if mid in sent:
                     continue
 
                 result = analyze_match(m)
 
                 if result:
-
                     send_message(result)
-
-                    sent_matches.add(mid)
-
-                    count += 1
-
-                    time.sleep(3)
+                    sent.add(mid)
 
             time.sleep(600)
 
         except Exception as e:
-            print(e)
+            print("ERROR:", e)
             time.sleep(30)
 
 
