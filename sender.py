880fabe8809b30f18a044d63afb9d164a2feaae8
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-24 20:57:09 +0300

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,21 +1,77 @@
-from telegram import Bot
-from config import BOT_TOKEN
+from telegram import (
+    Bot,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
 
-print("TOKEN:", BOT_TOKEN)
+from config import BOT_TOKEN, CHAT_ID
 
 bot = Bot(token=BOT_TOKEN)
 
+# =========================================
+# BUTONLU MESAJ
+# =========================================
+
+def send_panel():
+
+    keyboard = [
+
+        [
+            InlineKeyboardButton(
+                "📊 Canlı Maçlar",
+                callback_data="live"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "🔥 Bankolar",
+                callback_data="banko"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "⚽ KG VAR",
+                callback_data="kgvar"
+            ),
+
+            InlineKeyboardButton(
+                "🚀 ÜST 2.5",
+                callback_data="over25"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "🏆 Büyük Ligler",
+                callback_data="bigleagues"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "📈 Günün Kuponu",
+                callback_data="coupon"
+            )
+        ]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    bot.send_message(
+        chat_id=CHAT_ID,
+        text="🤖 PRO IDDAA ANALİZ PANEL",
+        reply_markup=reply_markup
+    )
+
+# =========================================
+# NORMAL MESAJ
+# =========================================
+
 def send_message(text):
 
-    try:
-
-        bot.send_message(
-            chat_id=CHAT_ID,
-            text=text
-        )
-
-        print("MESAJ GÖNDERİLDİ")
-
-    except Exception as e:
-
-        print("TELEGRAM ERROR:", e)
+    bot.send_message(
+        chat_id=CHAT_ID,
+        text=text
+    )
