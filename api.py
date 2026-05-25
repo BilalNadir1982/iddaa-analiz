ea92fe19f12637bcfc260ce87fd9a6bb4a511cff
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-25 19:55:39 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,17 +1,35 @@
 import requests
 from config import API_KEY
 
-URL = "https://v3.football.api-sports.io/fixtures"
-
-HEADERS = {
-    "x-apisports-key": API_KEY
-}
-
 def get_matches():
 
-    r = requests.get(URL, headers=HEADERS)
+    url = "https://v3.football.api-sports.io/fixtures?live=all"
 
-    if r.status_code != 200:
-        return []
+    headers = {
+        "x-apisports-key": API_KEY
+    }
 
-    return r.json().get("response", [])
+    response = requests.get(url, headers=headers)
+
+    data = response.json()
+
+    print(data)
+
+    matches = []
+
+    if "response" not in data:
+        return matches
+
+    for m in data["response"]:
+
+        match = {
+            "home": m["teams"]["home"]["name"],
+            "away": m["teams"]["away"]["name"],
+            "home_goals": m["goals"]["home"] or 0,
+            "away_goals": m["goals"]["away"] or 0,
+            "minute": m["fixture"]["status"]["elapsed"] or 0
+        }
+
+        matches.append(match)
+
+    return matches
