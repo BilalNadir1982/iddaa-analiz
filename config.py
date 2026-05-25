54d610f7393b2518744d3027a2c09c084bccb8f0
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-25 20:00:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("8625922357:AAEnTvRQU0YKAok-hA60izWCIS0uBpFjU4I")
-CHAT_ID = os.getenv("768262682")
-API_KEY = os.getenv("46b88d981d01e9ca3841ba6e6029e4cd")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+API_KEY = os.getenv("API_KEY")
