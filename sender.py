c7e6755d92fe14dc0e98024928f70eaed9b5fbff
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-25 19:57:41 +0300

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,27 +1,19 @@
-import os
 from telegram import Bot
+from config import BOT_TOKEN, CHAT_ID
 
-# ENV değişkenleri
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-# Güvenlik kontrolü (crash yerine net hata verir)
-if not BOT_TOKEN:
-    raise Exception("❌ BOT_TOKEN bulunamadı! GitHub Secrets kontrol et.")
-
-if not CHAT_ID:
-    raise Exception("❌ CHAT_ID bulunamadı! GitHub Secrets kontrol et.")
-
-# Bot başlat
 bot = Bot(token=BOT_TOKEN)
 
+def send_telegram(message):
 
-def send_message(text):
     try:
+
         bot.send_message(
             chat_id=CHAT_ID,
-            text=text,
-            parse_mode="HTML"
+            text=message
         )
+
+        print(message)
+
     except Exception as e:
-        print("Telegram mesaj hatası:", e)
+
+        print(e)
