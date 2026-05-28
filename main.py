23bed6429d82d9f7c1d1dd7204125447fb14bd56
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-28 12:52:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,63 @@
 ```python
 # main.py
 
+from api import get_matches
 from analyzer import analyze_match
 from coupon import create_coupon
 from sender import send_coupon
 
 # =========================================
-# SAMPLE DATA
-# API'DEN GELECEK
+# GET MATCHES
 # =========================================
 
-matches_data = [
+matches = get_matches()
 
-    {
-        "home": {
-            "name": "Galatasaray",
-            "wins_last5": 4,
-            "points_last5": 12,
-            "home_winrate": 80,
-            "goals_scored_avg": 2.3,
-            "first_half_goal_rate": 75,
-            "h2h_winrate": 70,
-            "missing_players": 0,
-            "motivation": "title",
-            "clean_sheet_rate": 60
-        },
+analyzed = []
 
-        "away": {
-            "name": "Kasımpaşa",
-            "wins_last5": 1,
-            "points_last5": 4,
-            "away_lossrate": 65,
-            "goals_scored_avg": 1.1,
-            "missing_players": 3
+for m in matches:
+
+    try:
+
+        fake_match = {
+
+            "home": {
+                "name": m["home"],
+                "wins_last5": 4,
+                "points_last5": 11,
+                "home_winrate": 75,
+                "goals_scored_avg": 2.1,
+                "first_half_goal_rate": 70,
+                "h2h_winrate": 65,
+                "missing_players": 1,
+                "motivation": "title",
+                "clean_sheet_rate": 55
+            },
+
+            "away": {
+                "name": m["away"],
+                "wins_last5": 1,
+                "points_last5": 4,
+                "away_lossrate": 65,
+                "goals_scored_avg": 1.0,
+                "missing_players": 3
+            }
         }
-    }
 
-]
+        result = analyze_match(fake_match)
 
-# =========================================
-# ANALYZE
-# =========================================
+        analyzed.append(result)
 
-analyzed_matches = []
-
-for match in matches_data:
-
-    result = analyze_match(match)
-
-    analyzed_matches.append(result)
+    except Exception as e:
+        print(e)
 
 # =========================================
 # CREATE COUPON
 # =========================================
 
-coupon = create_coupon(analyzed_matches)
+coupon = create_coupon(analyzed)
 
 # =========================================
-# SEND TELEGRAM
+# SEND
 # =========================================
 
 send_coupon(coupon)
