e1bfa4901089cf621cb8a4fc65f044fe04e8b4a2
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-28 15:30:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,23 @@ from api import fetch_daily_matches
 from analyzer import analyze_matches
 from coupon import format_coupon
 from sender import send_coupon
+from db import son_kuponu_kaydet  # Yeni ekledik
 
 def main():
     print("Bot çalıştırıldı, analiz süreci başlıyor...")
-    
-    # 1. Adım: Maç verilerini al
     raw_matches = fetch_daily_matches()
-    
-    # 2. Adım: Filtrele
     analyzed_matches = analyze_matches(raw_matches)
     
-    # EĞER MAÇ YOKSA TELEGRAM'A MESAJ ATMA, SESSİZCE ÇIK
     if not analyzed_matches or len(analyzed_matches) < 5:
-        print("Kriterlere uygun yeterli maç bulunamadı. Telegram'a mesaj gönderilmeyecek.")
-        return # Programı burada bitirir, kanala gereksiz yazı atmaz.
+        print("Kriterlere uygun yeterli maç bulunamadı.")
+        return 
     
-    # 3. Adım: Maç varsa kuponu hazırla
+    # Kuponu gönder
     coupon_text = format_coupon(analyzed_matches)
-    
-    # 4. Adım: Kanala gönder
-    print("Kupon hazırlandı, Telegram'a gönderiliyor...")
     send_coupon(coupon_text)
     
+    # Gece kontrolü için bu kuponu hafızaya yazıyoruz
+    son_kuponu_kaydet(analyzed_matches)
     print("İşlem başarıyla tamamlandı!")
 
 if __name__ == "__main__":
