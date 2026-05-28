09d47f7728e1aaba3e4dda8f49dacaeccf3043b9
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-28 13:00:27 +0300

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,68 +1,25 @@
-from api import get_matches
-from analyzer import analyze_match
-from coupon import create_coupon
-from sender import send_coupon
+from telegram import Bot
+from config import BOT_TOKEN, CHAT_ID
 
-# =========================================
+bot = Bot(token=BOT_TOKEN)
 
-# GET MATCHES
+def send_coupon(coupon):
 
-# =========================================
+    if not coupon:
+        bot.send_message(CHAT_ID, "BANKO MAÇ YOK")
+        return
 
-matches = get_matches()
+    text = "🏆 BANKO KUPON\n\n"
 
-analyzed = []
+    for m in coupon:
 
-for m in matches:
+        text += f"{m['home']} vs {m['away']}\n"
+        text += f"Güven: %{m['score']}\n"
+        text += f"Tip: {m['signal']}\n"
 
-```
-try:
+        for r in m["reasons"]:
+            text += f"- {r}\n"
 
-    fake_match = {
+        text += "\n"
 
-        "home": {
-            "name": m["home"],
-            "wins_last5": 4,
-            "points_last5": 11,
-            "home_winrate": 75,
-            "goals_scored_avg": 2.1,
-            "first_half_goal_rate": 70,
-            "h2h_winrate": 65,
-            "missing_players": 1,
-            "motivation": "title",
-            "clean_sheet_rate": 55
-        },
-
-        "away": {
-            "name": m["away"],
-            "wins_last5": 1,
-            "points_last5": 4,
-            "away_lossrate": 65,
-            "goals_scored_avg": 1.0,
-            "missing_players": 3
-        }
-    }
-
-    result = analyze_match(fake_match)
-
-    analyzed.append(result)
-
-except Exception as e:
-    print(e)
-```
-
-# =========================================
-
-# CREATE COUPON
-
-# =========================================
-
-coupon = create_coupon(analyzed)
-
-# =========================================
-
-# SEND
-
-# =========================================
-
-send_coupon(coupon)
+    bot.send_message(CHAT_ID, text)
