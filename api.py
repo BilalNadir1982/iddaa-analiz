cf3ce5559bffa8db5d98caef9dd6391986744e42
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-28 13:23:13 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,37 +1,17 @@
 import requests
 from config import API_KEY
 
-BASE_URL = "https://api.football-data.org/v4"
+BASE_URL = "https://v3.football.api-sports.io"
 
 HEADERS = {
-    "X-Auth-Token": API_KEY
+    "x-apisports-key": API_KEY
 }
 
-LEAGUES = ["PL", "SA", "BL1", "PD", "FL1", "CL"]
+def get_fixtures(league=39):
 
-def get_matches():
+    url = f"{BASE_URL}/fixtures?league={league}&season=2024"
 
-    matches = []
+    r = requests.get(url, headers=HEADERS)
+    data = r.json()
 
-    for league in LEAGUES:
-
-        url = f"{BASE_URL}/competitions/{league}/matches"
-
-        try:
-            r = requests.get(url, headers=HEADERS)
-            data = r.json()
-
-            for m in data.get("matches", []):
-
-                if m["status"] != "TIMED":
-                    continue
-
-                matches.append({
-                    "home": m["homeTeam"]["name"],
-                    "away": m["awayTeam"]["name"]
-                })
-
-        except:
-            pass
-
-    return matches
+    return data["response"]
