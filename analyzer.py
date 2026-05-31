75786afd38f90b4909e9cd95449932dc78a7e323
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-31 20:10:36 +0300

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,6 @@
 def analyze_matches(match_list):
-    results = []
+    # İstatistiksel analiz katmanı
     for m in match_list:
-        results.append({
-            "league": m["league"],
-            "home": m["home"],
-            "away": m["away"],
-            "prediction": "MS 1",
-            "confidence": 95
-        })
-    return results
+        m['prediction'] = "MS 1"
+        m['confidence'] = 85
+    return match_list
