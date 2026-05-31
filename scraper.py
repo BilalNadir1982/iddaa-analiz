b09393fd54f862995ea0e5eb6d8bb5a7fcd5ac59
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-31 20:22:30 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,11 +5,18 @@ from datetime import datetime
 def get_live_matches():
     api_key = os.getenv("FOOTBALL_API_KEY")
     today = datetime.now().strftime("%Y-%m-%d")
+    # API'den tüm ligleri çekiyoruz
     url = f"https://api.football-data.org/v4/matches?date={today}"
     headers = {"X-Auth-Token": api_key}
     
-    # İstediğin lig kodları
-    allowed_leagues = ["WC", "CL", "BL1", "DED", "BSA", "PD", "FL1", "ELC", "PPL", "EC", "SA", "PL"]
+    # Arjantin, Brezilya ve İspanya 2. Lig kodlarını ekledim
+    # ARJANTİN (Primera Nacional): 'ARG'
+    # BREZİLYA (Serie A): 'BSA'
+    # İSPANYA 2. LİG (Segunda Division): 'SD'
+    allowed_leagues = [
+        "WC", "CL", "BL1", "DED", "BSA", "PD", "FL1", 
+        "ELC", "PPL", "EC", "SA", "PL", "ARG", "SD"
+    ]
     
     try:
         response = requests.get(url, headers=headers).json()
