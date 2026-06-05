6c44921655e447ee66b0f23929e60f45fce60eed
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-06-05 22:32:29 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,15 +5,11 @@ from datetime import datetime, timedelta
 def get_live_matches():
     api_key = os.getenv("FOOTBALL_API_KEY")
     headers = {"X-Auth-Token": api_key}
-    # Sadece aktif ligler
     allowed = ["BSA", "ARG"] 
     
-    # Bugün ve Yarın için maçları çek
-    today = datetime.now().strftime("%Y-%m-%d")
-    tomorrow = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
-    
     matches = []
-    for date in [today, tomorrow]:
+    for days in range(2): # Bugün ve yarın
+        date = (datetime.now() + timedelta(days=days)).strftime("%Y-%m-%d")
         url = f"https://api.football-data.org/v4/matches?date={date}"
         try:
             response = requests.get(url, headers=headers).json()
@@ -25,4 +21,4 @@ def get_live_matches():
                         "away": m['awayTeam']['name']
                     })
         except: continue
-    return matches[:5] # En fazla 5 maç
+    return matches[:5]
